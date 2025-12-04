916e2cbf45cc71eae896d65b52c663199d360159
Sungmok-Jung <tjdahrwjd@gmail.com>
2025-12-04 14:25:36 +0000

    modify path

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cp -r KoNUBench/dataset/{kmmlu_neg,kmmlu_pos,kobest_boolq_neg,ko_nubench_cloze,ko_nubench_symbol} \
+cp -r /root/KoNUBench/lm_eval/{kmmlu_neg,kmmlu_pos,kobest_boolq_neg,ko_nubench_cloze,ko_nubench_symbol} \
       /root/sky_workdir/lm-evaluation-harness/lm_eval/tasks/
