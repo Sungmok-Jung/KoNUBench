04c21d71ecae915b01ff10d783133dbe399311f7
Sungmok-Jung <tjdahrwjd@snu.ac.kr>
2025-12-09 02:57:16 +0900

    wandb

--- sft/utils.py
+++ sft/utils.py
@@ -10,16 +10,16 @@ def sanitize_model_name(model_name: str) -> str:
 
 def wandb_init(args):
     wandb_name = f"{args.model}/lr{args.learning_rate}/seed{args.seed}"
-    checkpoint_path = f"{args.checkpoint_path}/{wandb_name}"
+    wandb_path = f"{args.checkpoint_path}/{sanitize_model_name(args.model)}-seed{args.seed}-lr{args.learning_rate}"
     if not args.wandb_off and args.global_rank == 0:
         wandb.login()
-        if not os.path.exists(f"{checkpoint_path}/wandb") :
-            os.makedirs(f"{checkpoint_path}/wandb")
+        if not os.path.exists(f"{wandb_path}/wandb") :
+            os.makedirs(f"{wandb_path}/wandb")
         run = wandb.init(
             project=args.wandb_project_name,
             entity=args.wandb_entity,
             name=wandb_name,
             config=args,
             save_code=True,
-            dir=checkpoint_path
+            dir=wandb_path
         )
